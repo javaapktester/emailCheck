75c00034dcd559c50bdcf7e78c3a4e57fca2d475
javaapktester <javaapktester@gmail.com>
2017-05-23 20:30:00 +0200

    Create mail_check.py

---
+++ mail_check.py
@@ -0,0 +1,23 @@
+#!/bin/python
+import poplib
+
+pop_conn = poplib.POP3_SSL('pop.gmail.com', '995')
+pop_conn.user('javaapktester')
+pop_conn.pass_('Avast2017!')
+
+numMessages = len(pop_conn.list()[1])
+for i in range(numMessages):
+    text = ''
+    for msg in pop_conn.retr(i+1)[1]:
+        text = text + msg + "\n"
+    p = FeedParser()
+    p.feed(text)
+    message = p.close()
+    print message['Message-ID']
+    print message['From']
+    print message['To']
+    print message['Bcc']
+
+    print message.get_payload()
+    
+pop_conn.quit()
