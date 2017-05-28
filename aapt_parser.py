c0a5c9b67301aebd0ca852f8362f1b61ee944cd3
marko <zbirka.marko@gmail.com>
2017-05-29 01:46:26 +0200

    first untested version, pair of TODOs left

---
+++ aapt_parser.py
@@ -0,0 +1,100 @@
+import smtplib
+import subprocess
+import os
+import argparse
+
+
+class RespContext(object):
+    def __init__(self):
+        self.name = ''
+        self.min_sdk = ''
+        self.target_sdk = ''
+        self.permissions = []
+        self.app_version = ''
+        self.launch_activity = ''
+        self.app_label = ''
+
+    # TODO: correct the output
+    def __str__(self):
+        return '''
+        Min SDK: 
+        Target SDK:
+        Permissions:
+        Package name:
+        App version:
+        Launchable-activity:
+        Application-label:
+        '''
+
+    def set_permissions(self, new_permission):
+        self.permissions.append(new_permission)
+
+
+class ApkParser(object):
+    def __init__(self, username, password, apk, resp_addr, debug=False):
+        self.username = username
+        self.password = password
+        self.apk = apk
+        self.resp_addr = resp_addr
+        self.debug = debug
+
+    def send_answer(self, text):
+        # TODO add subject
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(self.username, self.password)
+        if str(self.username).find('@gmail.com') > -1:
+            server.sendmail(self.username, self.resp_addr, text)
+        else:
+            server.sendmail(self.username+'@gmail.com', self.resp_addr, text)
+
+    def pars_sdk(self):
+        # run aapt on given path
+        try:
+            output = subprocess.check_output(["aapt", "dump", "badging", self.apk])
+            resp_data = RespContext()
+            # parse output
+            for line in output.decode("utf-8").splitlines():
+                split_line = line.split("'")
+                if split_line[0] == "package: name=":
+                    resp_data.name = split_line[1]
+                    resp_data.app_version = split_line[5]
+                    continue
+                if split_line[0] == "sdkVersion:":
+                    resp_data.min_sdk = split_line[1]
+                    continue
+                if split_line[0] == "targetSdkVersion:":
+                    resp_data.target_sdk = split_line[1]
+                    continue
+                if split_line[0] == "uses-permission: name=":
+                    resp_data.set_permissions(split_line[1])
+                    continue
+                if split_line[0] == "application-label:":
+                    resp_data.app_label = split_line[1]
+                    continue
+                if split_line[0] == "launchable-activity: name=":
+                    resp_data.launch_activity = split_line[1]
+                    continue
+
+            # send answer
+            self.send_answer(str(resp_data))
+            # delete dir
+            # TODO: delete the dir
+        # catch error
+        except subprocess.CalledProcessError as e:
+            if self.debug:
+                print(e.output)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='For given e-mail and path to apk file, it will run aapt for given '
+                                                 'apk and send parsed report to the given e-mail address.')
+    parser.add_argument("-u", "--username", type=str, help="google account login name")
+    parser.add_argument("-p", "--password", type=str, help="google account password")
+    parser.add_argument("-a", "--apk", type=str, help="apk file to be parsed.")
+    parser.add_argument("-r", "--resp", type=str, help="e-mail address, where to send the parsed information")
+    parser.add_argument("-v", "--verbose", action="store_true", help="show debug output")
+
+    args = parser.parse_args()
+    a = ApkParser(username=args.usrname, password=args.password, apk=args.apk, resp_addr=args.resp, debug=args.verbose)
+    a.pars_sdk()
