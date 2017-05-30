caedba9ece439a682ff5d3af56d5af937d8feb0d
marko <zbirka.marko@gmail.com>
2017-05-30 22:05:42 +0200

    added commentary for code

--- aapt_parser.py
+++ aapt_parser.py
@@ -6,6 +6,22 @@ import argparse
 
 
 class RespContext(object):
+    """Object of response context, with parsed properties of .apk
+
+        Instantiate with: RespContext(), it will init all values to empty string.
+
+        Parameters are set by direct calling.
+
+            name - Package name
+            min_sdk - Minimal SDK
+            target_sdk - Target SDK
+            permissions - list of permissions, to add permission, use set_permissions() object method.
+            app_version - App version
+            launch_activity - Launchable-activity
+            app_label - Application-label
+            file_name - filename of .apk file
+
+    """
     def __init__(self):
         self.name = ''
         self.min_sdk = ''
@@ -16,6 +32,7 @@ class RespContext(object):
         self.app_label = ''
         self.file_name = ''
 
+    # human readable string output of instance
     def __str__(self):
         return ''' 
         Filename: {}
@@ -30,8 +47,13 @@ class RespContext(object):
                    self.launch_activity, self.app_label, self.str_permissions())
 
     def set_permissions(self, new_permission):
+        """ append given permission to the instance list.
+
+        :param new_permission: string representation of parsed permission from .apk
+        """
         self.permissions.append(new_permission)
 
+    # human readable string output of permission list
     def str_permissions(self):
         x = ''
         for perm in self.permissions:
@@ -40,6 +62,18 @@ class RespContext(object):
 
 
 class ApkParser(object):
+    """Parser of .apk files, parsing output of aapt dump badging
+
+        Instantiate with: RespContext(username, password, apk, resp_addr, debug=False)
+
+
+            username - gmail account username
+            password - gmail account password
+            apk - apk file with absolute path e.g. /tmp/test.apk
+            resp_addr - e-mail address of the apk file sender
+            debug - verbosity of the instance, True for high, False(default) for low.
+
+        """
     def __init__(self, username, password, apk, resp_addr, debug=False):
         self.username = username
         self.password = password
@@ -48,59 +82,84 @@ class ApkParser(object):
         self.debug = debug
 
     def send_answer(self, text, subject):
+        """Universal method for sending e-mail answer, using instance variable
+
+        :param text: text part of the response
+        :param subject: subject of the e-mail response
+        """
+        # change the host:port for other mail-server if needed.
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
         server.login(self.username, self.password)
+        # create string representation of e-mail body with subject
         message = 'To: {}\nSubject: {}\n\n{}'.format(self.resp_addr, subject, text)
         if str(self.username).find('@gmail.com') > -1:
             server.sendmail(self.username, self.resp_addr, message)
         else:
+            # if using other then gmail account, change appropriately
             server.sendmail(self.username+'@gmail.com', self.resp_addr, message)
 
     def pars_sdk(self):
-        # run aapt on given path
+        """ run aapt on given path and create the RespContext instance, with parsed information
+
+        this method:
+            run and parse .apk file (from instance variable)
+            send the outcome information
+            delete apk from the server
+        """
         try:
             output = subprocess.check_output(["aapt", "dump", "badging", self.apk])
             resp_data = RespContext()
-            # parse output
+            # parse output, if the output of aapt would change in newer version, appropriated changes are needed
             for line in output.decode("utf-8").splitlines():
                 split_line = line.split("'")
+                # get package name and app version
                 if split_line[0] == "package: name=":
                     resp_data.name = split_line[1]
                     resp_data.app_version = split_line[5]
                     continue
+                # get min sdk version
                 if split_line[0] == "sdkVersion:":
                     resp_data.min_sdk = split_line[1]
                     continue
+                # get targeted sdk version
                 if split_line[0] == "targetSdkVersion:":
                     resp_data.target_sdk = split_line[1]
                     continue
+                # get permission
                 if split_line[0] == "uses-permission: name=":
                     resp_data.set_permissions(split_line[1])
                     continue
+                # get Application-label
                 if split_line[0] == "application-label:":
                     resp_data.app_label = split_line[1]
                     continue
+                # get Launchable-activity
                 if split_line[0] == "launchable-activity: name=":
                     resp_data.launch_activity = split_line[1]
                     continue
 
+            # get file name of the parsed apk
             resp_data.file_name = os.path.basename(self.apk)
             # send answer
             self.send_answer(str(resp_data), 'Re:APK')
-        # catch error
+        # catch error, if apk is not valid or damaged it will inform the sender about the situation.
         except subprocess.CalledProcessError as e:
             if self.debug:
                 print(e.output)
             self.send_answer("Not valid APK.")
 
+        # remove directory with .apk file
         shutil.rmtree(os.path.dirname(self.apk))
 
 if __name__ == "__main__":
+    # prepare for help and parsing arguments from terminal
     parser = argparse.ArgumentParser(description='For given e-mail and path to apk file, it will run aapt for given '
                                                  'apk and send parsed report to the given e-mail address.')
+    # to modify output if -h is used to mark arguments as required
     parser_group = parser.add_argument_group('required named arguments')
+
     parser_group.add_argument("-u", "--username", type=str, help="google account login name", required=True)
     parser_group.add_argument("-p", "--password", type=str, help="google account password", required=True)
     parser_group.add_argument("-a", "--apk", type=str, help="apk file to be parsed.", required=True)
@@ -108,5 +167,7 @@ if __name__ == "__main__":
     parser_group.add_argument("-v", "--verbose", action="store_true", help="show debug output")
 
     args = parser.parse_args()
+    # create instance of the parser with all needed inputs
     a = ApkParser(username=args.username, password=args.password, apk=args.apk, resp_addr=args.resp, debug=args.verbose)
+    # run & parse & send & delete
     a.pars_sdk()
--- test_mail_downloader.py
+++ test_mail_downloader.py
@@ -9,6 +9,21 @@ import argparse
 
 
 class GmailTest(object):
+    """Attachment parser for emails with specified subject and attachment extension.
+
+    This class checks new e-mail in inbox of specified e-mail(gmail) account, using poplib
+    and sends build command for Jenkins job with obtained information.
+
+    Instantiate with: GmailTest(g_username, g_password, j_username=None, j_password=None, debug=False)
+
+               g_username - username of google account
+               g_password - google account password
+               j_username - jenkins username, by default same as for google account
+               j_password - jenkins password, by default same as for google account
+               debug - verbosity of printable output
+
+        See the methods of the class for more documentation.
+    """
     def __init__(self, g_username, g_password, j_username=None, j_password=None, debug=False):
         self.debug = debug
         self.username = g_username
@@ -25,24 +40,39 @@ class GmailTest(object):
         else:
             self.jenPass = j_password
 
+        # Change following string to change path, where to store e-mails, remember the access rights
         self.savedir = "/home/user/javaapktester/emailCheck"
+        # Default Jenkins address and port, can by changed by set_jenkins_server method
         self.jenAddr = 'http://localhost:8080'
 
     def set_jenkins_server(self, address):
+        """
+        enable to specify other then default Jenkins' address and port, e.g. in case of remote Jenkins
+
+        :param address: the IP address and port on which Jenkins server is listening in format: XXX.XXX.XXX.XXX:PORT
+        """
         self.jenAddr = address
 
     def create_jenkins_job(self, email_addr, path):
         """
-        creating jenkins job for aapt and cleanup job
+        sending command for build of jenkins job for aapt and cleanup job
+
         :param email_addr: e-mail address of return address in the e-mail
         :param path: path to apk file
         """
         server = jenkins.Jenkins(self.jenAddr, username=self.jenUser, password=self.jenPass)
         server.build_job('apk_parser', {'email': email_addr[1:-1], 'apkPath': path})
 
-    def test_save_attach(self):
+    def save_attach(self):
+        """
+        Parse new e-mails in the inbox of GmailTest.g_username account.
+        Search for apk subject and download the attachment if it is of .apk extension.
+
+        Used parameter are from object instance, specified during __init__ or by set_jenkins_server() method.
+        """
         # TODO: add downloading attachment from gdrive
 
+        # To use other mail-server then gmail, just replace the host and port
         self.connection = poplib.POP3_SSL('pop.gmail.com', 995)
         if self.debug:
             self.connection.set_debuglevel(1)
@@ -73,22 +103,29 @@ class GmailTest(object):
             part_num = 0
             return_path = ''
 
+            # in the following loop, there is safety feature in case, wrong e-mail format will be obtained,
+            # it is not likely, but to be sure, if in the first part there is not Subject it will skip that part and try
+            # the next-one, etc. For this need there is part_num if its val is 1 program will be looking for subject.
+
             for part in str_message.walk():
                 part_num += 1
                 if part_num == 1:
                     if part.get('Subject', '') == '':
                         if self.debug:
-                            print("Haven't found the Subject keeping trying.")
+                            print("Haven't found the Subject, searching next part.")
                         part_num -= 1
                         continue
 
+                    # accepted Subject values are (a||A)&&(p||P)&&(k||K)
                     if part.get('Subject').upper() == 'APK':
+                        # find Return-Path information in the mail header
                         if part.get('Return-Path', '') != '':
                             return_path = part.get('Return-Path')
                             print(return_path)
                         else:
                             if self.debug:
                                 print('Unable to get return-path, using from value instead')
+                            # if Return-Path is missing, use From address from, mail header
                             if part.get('From', '') == '':
                                 if self.debug:
                                     print('Unable to parse return address.')
@@ -98,10 +135,12 @@ class GmailTest(object):
                                 return_path = part.get('From')
                                 print(return_path)
 
+                # when we know where to send response, start searching for .apk
                 if return_path != '':
                     if self.debug:
                         print(part.get_content_type())
 
+                    # if true, then its parts will be walk in following iterations.
                     if part.get_content_maintype() == 'multipart':
                         continue
 
@@ -111,7 +150,9 @@ class GmailTest(object):
                             print(part.get_payload())
                         continue
 
+                    # Parsing original filename
                     filename = part.get_filename()
+                    # check if it is .apk
                     if os.path.splitext(filename)[-1] == '.apk':
                         print("Filename: " + str(filename))
                     else:
@@ -119,29 +160,38 @@ class GmailTest(object):
                             print('Attached file is not .apk')
                         continue
 
+                    # create directory where to store the attached .apk, dirname will be actual timestamp, without dot.
                     dir_path = os.path.join(self.savedir, str(time.time()).replace(".", ""))
                     os.mkdir(dir_path)
+
+                    # concat the path to file with normalized filename
+                    # normalized means: if filename contained whitespace, replace it with underscore
                     path = os.path.join(dir_path, filename.replace(' ', '_'))
 
+                    # write attachment do file specified in path variable.
                     fp = open(path, 'wb')
                     fp.write(part.get_payload(decode=1))
                     fp.close()
 
+                    # send command to Jenkins server, to build a second job with given return address and apk file.
                     self.create_jenkins_job(return_path, path)
             # remove processed e-mail, for sanity of the mailbox, I suggest to uncomment next line, but it is not needed
             #self.connection.dele(i + 1)
 
-            print('#####################################')
+            if self.debug:
+                print('#####################################')
 
+        # close the connection to gmail pop server, if commented out, the e-mails won't be marked as parsed.
         self.connection.quit()
 
 
 if __name__ == '__main__':
+    # prepare for help and parsing arguments from terminal
     parser = argparse.ArgumentParser(description='Test if for given gmail acc, there is any unprocessed e-mails, '
                                                  'with specified subject, if so download the attachment and create '
                                                  'the jenkins job.')
-    parser.add_argument("-u", "--username", type=str, help="google account login name")
-    parser.add_argument("-p", "--password", type=str, help="google account password")
+    parser.add_argument("-u", "--username", type=str, help="google account login name", required=True)
+    parser.add_argument("-p", "--password", type=str, help="google account password", required=True)
     parser.add_argument("-U", "--jenusername", type=str, default=None,
                         help="jenkins account login name [default same as for google]")
     parser.add_argument("-P", "--jenpassword", type=str, default=None,
@@ -151,9 +201,11 @@ if __name__ == '__main__':
     parser.add_argument("-v", "--verbose", action="store_true", help="show debug output")
     args = parser.parse_args()
 
+    # create instance of GmailTest and set all needed parameters
     d = GmailTest(g_username=args.username, g_password=args.password, j_username=args.jenusername,
                   j_password=args.jenpassword, debug=args.verbose)
+    # if needed uncomment and modify following line
+    # d.set_jenkins_server('192.168.1.1:8400')
 
-
-
-    d.test_save_attach()
+    # start parsing && saving attachments
+    d.save_attach()
